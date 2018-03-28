234f420f4d3e1e864b2ebf507128a6b00188638a
Kuvar Singh <kuvarjava@gmail.com>
2018-03-28 23:08:03 +0530

    CustomRenderer
    CustomRenderer

---
+++ FontawesomeControls/FontawesomeControls.Android/CustomRenderer/FontawesomeLabelRenderer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using FontawesomeControls.Droid.CustomRenderer;
+using FontawesomeControls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics;
+
+[assembly: ExportRenderer(typeof(FontawesomeLabel), typeof(FontawesomeLabelRenderer))]
+[assembly: ExportRenderer(typeof(FontawesomeButton), typeof(FontawesomeButtonRenderer))]
+namespace FontawesomeControls.Droid.CustomRenderer
+{
+    public class FontawesomeLabelRenderer : LabelRenderer
+    {
+        public FontawesomeLabelRenderer(Context context):base(context)
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+                AwesomeUtil.CheckAndSetTypeFace(Control);
+        }
+    }
+
+    public class FontawesomeButtonRenderer : ButtonRenderer
+    {
+        public FontawesomeButtonRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+                AwesomeUtil.CheckAndSetTypeFace(Control);
+        }
+    }
+
+    internal static class AwesomeUtil
+    {
+        public static void CheckAndSetTypeFace(TextView view)
+        {
+            if (view.Text.Length == 0) return;
+            var text = view.Text;
+
+            var font = Typeface.CreateFromAsset(Forms.Context.ApplicationContext.Assets, "fontawesome.ttf");
+            view.Typeface = font;
+        }
+    }
+}
---
+++ FontawesomeControls/FontawesomeControls.iOS/CustomRenderer/FontawesomeLabelRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FontawesomeControls.iOS.CustomRenderer;
+using FontawesomeControls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+
+[assembly: ExportRenderer(typeof(FontawesomeLabel), typeof(FontawesomeLabelRenderer))]
+[assembly: ExportRenderer(typeof(FontawesomeButton), typeof(FontawesomeButtonRenderer))]
+namespace FontawesomeControls.iOS.CustomRenderer
+{
+    public class FontawesomeLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null || Element == null)
+                return;
+            Control.Font = UIFont.FromName("FontAwesome", (nfloat)e.NewElement.FontSize);
+        }
+    }
+
+    public class FontawesomeButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null || Element == null)
+                return;
+            Control.Font = UIFont.FromName("FontAwesome", (nfloat)e.NewElement.FontSize);
+        }
+    }
+}
